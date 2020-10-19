618ad3af02023bd70981196b8ddb002062307b5c
GeorgeLeeo <georgeleeo@163.com>
2020-10-19 19:29:25 +0800

    Update TabBar.vue

--- components/TabBar/TabBar.vue
+++ components/TabBar/TabBar.vue
@@ -1,7 +1,8 @@
 <template>
 	<view class="tab-bar">
 		<view v-for="(item, index) in tabList" :key="item.text" class="tab-item">
-			<text>{{item.text}}</text> 
+			<image class="tab-icon" :src="item.selected" mode="aspectFill"></image>
+			<text class="tab-text">{{item.text}}</text> 
 		</view>
 	</view>
 </template>
@@ -32,9 +33,23 @@
 	height: 90rpx;
 	align-items: center;
 	justify-content: space-around;
-	background-color: #007AFF;
+	background-color: #fff;
+	border-top: 1px solid $uni-text-color-grey;
 }
 .tab-item {
 	width: 100%;
+	text-align: center;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	justify-content: center;
+}
+.tab-text {
+	font-size: $uni-font-size-sm;
+	color: $uni-text-color-grey;
+}
+.tab-icon {
+	width: 30rpx;
+	height: 30rpx;
 }
 </style>
